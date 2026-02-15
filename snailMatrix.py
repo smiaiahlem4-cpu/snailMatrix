1e5588fc3b19672e20aa0725e54ff5afe0329df9
smiaiahlem4-cpu <smiaiahlem4@gmail.com>
2026-02-15 01:15:39 +0100

    Update snailMatrix.py

--- snailMatrix.py
+++ snailMatrix.py
@@ -1,6 +1,7 @@
-# generate_snail_matrix.py
+
 
 def generate_spiral(n):
+
     matrix = [[0]*n for _ in range(n)]
     
     left, right = 0, n-1
@@ -8,27 +9,26 @@ def generate_spiral(n):
     num = 1
     
     while left <= right and top <= bottom:
-        
-        # Top row
+   
         for i in range(left, right+1):
             matrix[top][i] = num
             num += 1
         top += 1
         
-        # Right column
+     
         for i in range(top, bottom+1):
             matrix[i][right] = num
             num += 1
         right -= 1
         
-        # Bottom row
+       
         if top <= bottom:
             for i in range(right, left-1, -1):
                 matrix[bottom][i] = num
                 num += 1
             bottom -= 1
         
-        # Left column
+      
         if left <= right:
             for i in range(bottom, top-1, -1):
                 matrix[i][left] = num
@@ -37,10 +37,10 @@ def generate_spiral(n):
             
     return matrix
 
-
 if __name__ == "__main__":
-    n = int(input("n :"))
+    n = int(input("Enter the matrix size n: "))
     result = generate_spiral(n)
     
-    # Print full matrix as a 2D list
-    print(result)
+    # Print matrix row by row
+    for row in result:
+        print(row)
